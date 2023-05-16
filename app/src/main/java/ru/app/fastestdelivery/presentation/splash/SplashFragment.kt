d621753440ac0be8fc751f32971d7664d73d882a
an.nikolaev <an.nikolaev@okko.tv>
2023-05-16 15:26:59 +0300

    Implemented SplashFragment

--- app/src/main/java/ru/app/fastestdelivery/presentation/splash/SplashFragment.kt
+++ app/src/main/java/ru/app/fastestdelivery/presentation/splash/SplashFragment.kt
@@ -1,5 +1,7 @@
 package ru.app.fastestdelivery.presentation.splash
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -12,6 +14,13 @@ class SplashFragment : Fragment(R.layout.fragment_splash) {
 
     private val viewBinding: FragmentSplashBinding by viewBinding(FragmentSplashBinding::bind)
     private val viewModel: SplashViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.splashContinueButton.setOnClickListener {
+            viewModel.continueButtonClicked()
+        }
+    }
 
     companion object {
 
--- app/src/main/java/ru/app/fastestdelivery/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/ru/app/fastestdelivery/presentation/splash/SplashViewModel.kt
@@ -1,9 +1,18 @@
 package ru.app.fastestdelivery.presentation.splash
 
 import androidx.lifecycle.ViewModel
+import com.github.terrakok.cicerone.Router
 import dagger.hilt.android.lifecycle.HiltViewModel
+import ru.app.fastestdelivery.presentation.screens.Screens
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(): ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val router: Router
+): ViewModel() {
+
+    fun continueButtonClicked() {
+        router.navigateTo(Screens.loginScreen())
+    }
+
 }
