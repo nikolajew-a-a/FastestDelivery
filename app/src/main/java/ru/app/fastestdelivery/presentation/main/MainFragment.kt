b2a36190f200d03285b965ef564333f778df9541
an.nikolaev <an.nikolaev@okko.tv>
2023-05-18 06:51:55 +0300

    Implemented bottom_navigation_view

--- app/src/main/java/ru/app/fastestdelivery/presentation/main/MainFragment.kt
+++ app/src/main/java/ru/app/fastestdelivery/presentation/main/MainFragment.kt
@@ -1,5 +1,7 @@
 package ru.app.fastestdelivery.presentation.main
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -12,6 +14,27 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val viewBinding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() = with(viewBinding) {
+        mainBottomNavigation.setOnNavigationItemReselectedListener { item ->
+            when(val itemId = item.itemId) {
+                R.id.menu_home -> {
+                    // Respond to navigation item 1 reselection
+                }
+                R.id.menu_bag -> {
+                    // Respond to navigation item 2 reselection
+                }
+                R.id.menu_profile -> {
+                    // Respond to navigation item 2 reselection
+                }
+            }
+        }
+    }
 
     companion object {
 
